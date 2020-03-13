f87ec8a806da0d57879e52bea183711424562ede
Kyzero666 <38668184+Kyzero666@users.noreply.github.com>
2020-03-13 20:55:53 +0800

    Update cptprice.js

--- cptprice.js
+++ cptprice.js
@@ -100,39 +100,38 @@ cptprice.on('message', function(message) {
         message.channel.send('I got nothing to say')
       }
     }
-
-    if (msg.startsWith(prefix + 'WEATHER')) {
-
-
-        weather.find({search: args.join(" "), degreeType: 'F'}, function(err, result) {
-            if (err) message.channel.send(err);
-
-            if (result === undefined || result.length === 0) {
-                message.channel.send('**Please enter a valid location.**')
-                return;
-            }
-
-             Variables
-            var current = result[0].current;
-            var location = result[0].location;
+    //if (msg.startsWith(prefix + 'WEATHER')) {
 
 
-            const embed = new Discord.RichEmbed()
-                .setDescription(`**${current.skytext}**`)
-              .setAuthor(`Weather for ${current.observationpoint}`)
-                .setThumbnail(current.imageUrl)
-                .setColor(0x00AE86)
-                .addField('Timezone',`UTC${location.timezone}`, true)
-                .addField('Degree Type',location.degreetype, true)
-                .addField('Temperature',`${current.temperature} Degrees`, true)
-                .addField('Feels Like', `${current.feelslike} Degrees`, true)
-                .addField('Winds',current.winddisplay, true)
-                .addField('Humidity', `${current.humidity}%`, true)
+    //    weather.find({search: args.join(" "), degreeType: 'F'}, function(err, result) {
+    //        if (err) message.channel.send(err);
+
+    //        if (result === undefined || result.length === 0) {
+    //            message.channel.send('**Please enter a valid location.**')
+    //            return;
+    //        }
+
+            // Variables
+   //         var current = result[0].current;
+    //        var location = result[0].location;
 
 
-                message.channel.send({embed});
-        });
-    }
+    //        const embed = new Discord.RichEmbed()
+     //           .setDescription(`**${current.skytext}**`)
+      //          .setAuthor(`Weather for ${current.observationpoint}`)
+       //         .setThumbnail(current.imageUrl)
+        //        .setColor(0x00AE86)
+         //       .addField('Timezone',`UTC${location.timezone}`, true)
+          //      .addField('Degree Type',location.degreetype, true)
+           //     .addField('Temperature',`${current.temperature} Degrees`, true)
+            //    .addField('Feels Like', `${current.feelslike} Degrees`, true)
+             //   .addField('Winds',current.winddisplay, true)
+              //  .addField('Humidity', `${current.humidity}%`, true)
+
+
+          //      message.channel.send({embed});
+      //  });
+   // }
 
 });
 
